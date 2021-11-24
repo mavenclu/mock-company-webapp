8ea65e865a203dc786f4d423c07cd6319607a5a2
Marie Venclu <yakovma1@fel.cvut.cz>
2021-11-24 18:55:12 +0100

    refactored runReport method

--- src/main/java/com/mockcompany/webapp/controller/ReportController.java
+++ src/main/java/com/mockcompany/webapp/controller/ReportController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityManager;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,40 +17,31 @@ import java.util.Map;
 @RestController
 public class ReportController {
 
-    // After reading code/tests, we can capture the important terms in an array!
-    private static final String[] importantTerms = new String[] {
+    private static final String[] importantTerms = new String[]{
             "Cool",
             "Amazing",
             "Perfect",
             "Kids"
     };
 
-    private final EntityManager entityManager;
-    // Declare SearchService same as EntityManager
     private final SearchService searchService;
 
-    // Add the SearchService to the constructor
     @Autowired
-    public ReportController(EntityManager entityManager, SearchService searchService) {
-        this.entityManager = entityManager;
+    public ReportController(SearchService searchService) {
         this.searchService = searchService;
     }
 
+
     @GetMapping("/api/products/report")
     public SearchReportResponse runReport() {
-        // We could use the search service and do an empty string query to get the count but this is much more efficient
-        Number count = (Number) this.entityManager.createQuery("SELECT count(item) FROM ProductItem item").getSingleResult();
-
-        // For each important term, query on it and add size of results to our Map
         Map<String, Integer> hits = new HashMap<>();
         for (String term : importantTerms) {
             hits.put(term, searchService.search(term).size());
         }
-
-        // Transform to API response and return
         SearchReportResponse response = new SearchReportResponse();
-        response.setProductCount(count.intValue());
         response.setSearchTermHits(hits);
+        response.setProductCount(searchService.findAll().size());
         return response;
     }
+
 }
